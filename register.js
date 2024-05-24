e3a02e604fc6261d6b5ef3860fdf1605ea0b9ab0
Николай <karta5269@gmail.com>
2024-05-24 21:54:55 +0300

    gitignor is working?

--- register.js
+++ register.js
@@ -4,56 +4,42 @@ const path = require("path");
 const bodyParser = require('body-parser');
 const register = express.Router();;
 const util = require('util');
+const { Client} = require('pg');
 const { validator } = require('validator');
 const { check, oneOf, validationResult } = require('express-validator');
 const exp = require("constants");
 
 
-const validation = [
-    oneOf([
-        check('nickname')
-          .exists()
-          .withMessage('nickname is required')
-          .isLength({ min: 3 })
-          .withMessage('wrong nickname length'),
+const client = new Client({
+	user: 'postgres',
+	password: 'postgres',
+	host: 'localhost',
+	port: '5432',
+	database: 'register',
+});
 
-        check('nickname')
-          .exists()
-          .withMessage('nickname is required')
-          .isEmail()
-          .withMessage('nickname not valid'),
-    ]),
-    check('password')
-        .exists()
-        .withMessage('password is required')
-];
-
-function handleValidationErrors(req, res, next) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(util.inspect(errors.array()));
-    return res.status(402).json({ errors: errors.array() });
-  }
-
-  next();
-};
-
-// router
-//   .post('/', validation, handleValidationErrors,
-//     (req, res) => {
-//       const isEmail = validator.isEmail(req.body.nickname);
-
-//       res.status(200).json({ isEmail });
-//     });
+client
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
 
 register.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'publick', 'registerPage.html'))
 });
 
-register.post('/', validation, handleValidationErrors, (req, res) => {
-    const login = validator.isEmail(req.body.login);
-    const {password} = req.body
-    res.send(login)
-})
+register.post('/', async (req, res) => {    
+    const { login, password } = req.body;
+    try {
+        await client.query('INSERT INTO registration(login, pass) VALUES ($1,$2)',[login, password]);
+    } catch (e) {
+        console.err(e)
+    }
+    
+    res.redirect(`/login`);
+});
 
 module.exports = register;
--- server.js
+++ server.js
@@ -3,11 +3,27 @@ const app = express();
 const path = require("path");
 const db = require('./db');
 const bodyParser = require('body-parser');
-
+const {Client} = require('pg');
 const register = require('./register.js');
-
+const { error } = require("console");
+const { check } = require('express-validator');
+const client = new Client({
+	user: 'postgres',
+	password: 'postgres',
+	host: 'localhost',
+	port: '5432',
+	database: 'register',
+});
 
 PORT = 4001;
+client
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -18,21 +34,33 @@ app.use(express.static(path.join(__dirname, 'publick')));
 app.use('/register', register); 
 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {    
     res.sendFile(path.join(__dirname, 'publick', 'gomapage.html'))
 });
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'publick', 'loginPage.html'))
 });
 
-app.post('/login', (req, res) => {
-    const login = req.body.login;    
-    res.send(`Zdarova ${login}`);
+app.post('/login',[
+	check('login').notEmpty().withMessage('Login is required'),
+	check('password').notEmpty().withMessage('Password is required')
+], async (req, res) => {
+    const {login, password} = req.body;
+    const user = await client.query('SELECT * FROM registration WHERE login = $1', [login]);
+    const loginInDB = user.rows[0].login;
+    const passwordInDB = user.rows[0].pass;    
+    
+    if (login === loginInDB && password === passwordInDB) {
+                res.send(`Zdarova ${login}`);  
+    } else {
+            res.status(404).send('opyat zalupa');
+    }                    
+    
 })
 
 
 
-app.listen(4001, () => console.log(`Сервер хуярит na 400 porte`));
+app.listen(4001, () => console.log(`Сервер хуярит na 4001 porte`));
 
 
 module.exports = register;
