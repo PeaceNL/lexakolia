bcd56561dd6457a8c27150ea29dac01442bd7259
Николай <karta5269@gmail.com>
2024-05-26 15:15:29 +0300

    add validation and async request

--- register.js
+++ register.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 const register = express.Router();;
 const util = require('util');
 const { Client} = require('pg');
-const { validator } = require('validator');
-const { check, oneOf, validationResult } = require('express-validator');
+const { check, validationResult} = require('express-validator');
 const exp = require("constants");
 const bcrypt = require('bcrypt');
 
@@ -28,28 +27,40 @@ client
 		console.error('Error connecting to PostgreSQL database', err);
 	});
 
+const loginPassValidation = [
+	check('login')
+	  .isAlphanumeric().withMessage('Логин может содержать только буквы и цифры')
+	  .isLength({ min: 3 }).withMessage('Логин должен быть длинее 3 символов'),
+		check('password')
+	  .isLength({ min: 6 }).withMessage('Пароль должен быть длинее 6 символов')
+	];
+
 register.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'publick', 'registerPage.html'))
 });
 
-register.post('/', async (req, res) => {    
-    const { login, password } = req.body;
+register.post('/', loginPassValidation, async (req, res) => {
+	const { login, password } = req.body;
+
+	const result = validationResult(req);
+	if (!result.isEmpty()){
+		res.status(400).json({message: result.errors[0].msg});
+		return;
+	}	
 	
     try {
 		const user = await client.query('SELECT * FROM registration WHERE login = $1', [login]);		
 		if (user.rows.length > 0) {
-			res.send(`Логин ${user.rows[0].login} уже существует`);
+			res.json({message: `Логин ${user.rows[0].login} уже существует`});
 		} else {
 			const hashPassword = await bcrypt.hash(password, 10);
 			await client.query('INSERT INTO registration(login, pass) VALUES ($1,$2)',[login, hashPassword]);
-			res.redirect(`/login`);
+			res.status(201).json({message: `Акаунт успешно создан!`});
 		}
         
     } catch (e) {
-        console.log(e);
+        return res.status(500).json({ message: 'Server error' });
     }
-    
-    
 });
 
 module.exports = register;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const {Client} = require('pg');
 const register = require('./register.js');
 const { error } = require("console");
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 
 const client = new Client({
@@ -43,24 +43,31 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'publick', 'loginPage.html'))
 });
 
-app.post('/login', async (req, res) => {
+app.post('/login',[
+	check('login', 'Заполните поле логина').notEmpty(),
+	check('password', "Ввведите пароль").notEmpty()
+], async (req, res) => {
     const {login, password} = req.body;
 	try {
+		const result = validationResult(req);
+		if (!result.isEmpty()) {
+			return res.status(400).json({message: result.errors[0].msg});			
+		}
     	const user = await client.query('SELECT * FROM registration WHERE login = $1', [login]);
 		if (user.rows.length > 0) {			
     		const passwordInDB = user.rows[0].pass;
 			const isMatch = await bcrypt.compare(password, passwordInDB);
 			if (isMatch) {
-				res.send(`Zdarova ${login}`);
+				res.status(202).json({message: `Zdarova ${login}`});
 			}  else {
-				res.status(404).send('Неверный пароль!');
+				res.status(404).json({message: 'Неверный пароль!'});
 			}   
 		} else {
-			res.status(404).send('User not found!');
+			res.status(404).json({message: 'User not found!'});
 		}
     	
 	} catch (e) {		
-		res.status(500).send('Ошибка сервера');
+		res.status(500).json({message: 'Ошибка сервера'});
 	}                  
     
 })
